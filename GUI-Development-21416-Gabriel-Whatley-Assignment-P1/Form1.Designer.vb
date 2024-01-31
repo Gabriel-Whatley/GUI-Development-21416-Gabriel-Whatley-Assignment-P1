550ef3f0ddb6ea93d4d055f0668a1786dd540ad0
Gabriel Whatley <whatley_gabriel@students.columbusstate.edu>
2024-01-31 14:35:06 -0500

    Saving just in case.

--- GUI-Development-21416-Gabriel-Whatley-Assignment-P1/Form1.Designer.vb
+++ GUI-Development-21416-Gabriel-Whatley-Assignment-P1/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class frmNumbers
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,93 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        lblFrench = New Label()
+        btnOne = New Button()
+        btnTwo = New Button()
+        btnThree = New Button()
+        btnFour = New Button()
+        btnFive = New Button()
+        SuspendLayout()
+        ' 
+        ' lblFrench
+        ' 
+        lblFrench.AutoSize = True
+        lblFrench.Location = New Point(226, 52)
+        lblFrench.Name = "lblFrench"
+        lblFrench.Size = New Size(259, 45)
+        lblFrench.TabIndex = 0
+        lblFrench.Text = "Do you know the French words for the numbers" & vbCrLf & "1 through 5?" & vbCrLf & "Click the buttons below to see them."
+        ' 
+        ' btnOne
+        ' 
+        btnOne.Location = New Point(53, 108)
+        btnOne.Name = "btnOne"
+        btnOne.Size = New Size(75, 23)
+        btnOne.TabIndex = 1
+        btnOne.Text = "1"
+        btnOne.UseVisualStyleBackColor = True
+        ' 
+        ' btnTwo
+        ' 
+        btnTwo.Location = New Point(134, 108)
+        btnTwo.Name = "btnTwo"
+        btnTwo.Size = New Size(75, 23)
+        btnTwo.TabIndex = 2
+        btnTwo.Text = "2"
+        btnTwo.UseVisualStyleBackColor = True
+        ' 
+        ' btnThree
+        ' 
+        btnThree.Location = New Point(215, 108)
+        btnThree.Name = "btnThree"
+        btnThree.Size = New Size(75, 23)
+        btnThree.TabIndex = 3
+        btnThree.Text = "3"
+        btnThree.UseVisualStyleBackColor = True
+        ' 
+        ' btnFour
+        ' 
+        btnFour.Location = New Point(296, 108)
+        btnFour.Name = "btnFour"
+        btnFour.Size = New Size(75, 23)
+        btnFour.TabIndex = 4
+        btnFour.Text = "4"
+        btnFour.UseVisualStyleBackColor = True
+        ' 
+        ' btnFive
+        ' 
+        btnFive.Location = New Point(377, 108)
+        btnFive.Name = "btnFive"
+        btnFive.Size = New Size(75, 23)
+        btnFive.TabIndex = 5
+        btnFive.Text = "5"
+        btnFive.UseVisualStyleBackColor = True
+        ' 
+        ' frmNumbers
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnFive)
+        Controls.Add(btnFour)
+        Controls.Add(btnThree)
+        Controls.Add(btnTwo)
+        Controls.Add(btnOne)
+        Controls.Add(lblFrench)
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "frmNumbers"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "French Numbers"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents lblFrench As Label
+    Friend WithEvents btnOne As Button
+    Friend WithEvents btnTwo As Button
+    Friend WithEvents btnThree As Button
+    Friend WithEvents btnFour As Button
+    Friend WithEvents btnFive As Button
 
 End Class
--- GUI-Development-21416-Gabriel-Whatley-Assignment-P1/Form1.vb
+++ GUI-Development-21416-Gabriel-Whatley-Assignment-P1/Form1.vb
@@ -1,3 +1,9 @@
-﻿Public Class Form1
+﻿Public Class frmNumbers
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub lblFrench_Click(sender As Object, e As EventArgs) Handles lblFrench.Click
+
+    End Sub
 End Class
